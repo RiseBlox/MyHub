8b74067435838e9a846b6ac32a4e615b0032a30a
RiseBlox <badimocc.3089@gmail.com>
2026-02-24 15:39:26 +0700

    v4.enhanced.jbfix (2/2)

--- MyHub.lua
+++ MyHub.lua
@@ -4,10 +4,24 @@
   /\/\  _   _  /\  /\_   _| |__
  /    \| | | |/ /_/ / | | | '_ \
 / /\/\ \ |_| / __  /| |_| | |_) |
-\/    \/\__, \/ /_/  \__,_|_.__/  v4.0
+\/    \/\__, \/ /_/  \__,_|_.__/  v4.enhanced
         |___/
 
-
 ]]
+
+local function sendNotification(text, duration, title)
+	if game.PlaceId == 606849621 then
+		require(game:GetService("ReplicatedStorage").Game.Notification).new({
+			Text = text,
+			Duration = duration
+		})
+	else
+		game:GetService("StarterGui"):SetCore("SendNotification", {
+			Title = title,
+			Text = text
+		})
+	end
+end
+sendNotification("Successfully loaded!", 2, "MyHub V4 [ENHANCED]")
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MyOrganizationStudio/MyHub/main/MyHub.luau", true))()
